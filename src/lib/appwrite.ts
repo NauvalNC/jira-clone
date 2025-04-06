ff45e51b7232c88a205661173215aaad39d0d02e
NauvalNC <nauvalmfirdaus@gmail.com>
2025-04-06 11:30:02 +0700

    fix: fix incorrect get cookie implementation due to next.js 15

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -8,7 +8,8 @@ export const createSessionClient = async () => {
     const client = new Client()
         .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
         .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-    const session = cookies().get(AUTH_COOKIE);
+    const webCookies = await cookies();
+    const session = webCookies.get(AUTH_COOKIE);
     if (!session || !session.value) {
         throw new Error("Unauthorized");
     }
